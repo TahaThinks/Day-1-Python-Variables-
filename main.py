fd46b5810775b17122e9a425aa7526a090e26228
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2023-01-29 20:27:59 +0000

    Swapping Variables Values

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 name = input("What is your name?")
+print(f"Hello, {name}\t Lets Play a Game")
+a= input("a: ")
+b= input("b: ")
+print("Swap Values of A & B\n")
+
+c=a
+a=b
+b=c
+
+print(f"Value of a:{a} & Value of b:{b}")
